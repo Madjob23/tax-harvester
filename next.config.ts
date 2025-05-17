85109c1acb772ce45f1be68692001b267f6d9c9b
madhav <smadhvendra20@gmail.com>
2025-05-17 20:47:20 +0530

    Edit config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'coin-images.coingecko.com',
+      'koinx-statics.s3.ap-south-1.amazonaws.com'
+    ],
+  },
 };
 
 export default nextConfig;
